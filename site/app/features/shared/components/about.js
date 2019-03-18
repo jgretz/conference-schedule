274c2f1edc6b28283daa81017a708ca13db3c448
Josh Gretz <jgretz@truefit.io>
2019-03-18 15:20:00 -0400

    clean up about page

--- site/app/features/shared/components/about.js
+++ site/app/features/shared/components/about.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import {withStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Divider from '@material-ui/core/Divider';
 
 const styles = {
   root: {
     width: '96%',
     maxWidth: 800,
     margin: '20px auto 0 auto',
+  },
+
+  text: {
+    marginBottom: 24,
+  },
+
+  divider: {
+    marginBottom: 24,
+    marginTop: 24,
   },
 
   coffee: {
@@ -58,28 +69,38 @@ const About = ({classes}) => (
     <Typography variant="h4" align="center" gutterBottom>
       Alternative Conference Schedule
     </Typography>
-    <Typography variant="body1" gutterBottom>
-      I am fortunate to be able to attend quite a few conferences each year. I
-      have found that a big key to having a successful experience is to take the
-      time to figure out what talks pique your interest beforehand, so you can
-      quickly adjust on the fly as the conference unfolds. The thing that most
-      often gets in the way of this goal is that each conference publishes the
-      schedule in a slightly different way. Some provide apps or sites that save
-      your favorites, while others just put up a read only page - often in an
-      unfortunate format that requires both horizontal and vertical scrolling.
+    <Typography variant="body1" gutterBottom className={classes.text}>
+      Hi, I&apos;m Josh Gretz (
+      <Link href="https://twitter.com/joshgretz" target="_blank">
+        @joshgretz
+      </Link>
+      ), I&apos;m the CTO at{' '}
+      <Link href="https://www.truefit.io" target="_blank">
+        Truefit
+      </Link>
+      , and I built this site.
     </Typography>
-    <Typography variant="body1" gutterBottom>
+    <Typography variant="body1" gutterBottom className={classes.text}>
+      As part of my role, I am fortunate to be able to attend quite a few
+      conferences each year. In my experience, a key to having a fruitful
+      experience is to take the time to figure out what sessions pique your
+      interest beforehand. This approach allows you not only to plan a good mix,
+      but also enables you to quickly adjust on the fly as the conference
+      unfolds.
+    </Typography>
+    <Typography variant="body1" gutterBottom className={classes.text}>
+      Every conference, though, publishes their schedule in a slightly different
+      way. Some provide apps or sites that save your favorites, while others
+      just put up a read only page - often in an unfortunate format that
+      requires both horizontal and vertical scrolling.
+    </Typography>
+    <Typography variant="body1" gutterBottom className={classes.text}>
       To solve this problem, I created this site to standardize the schedules of
       conferences I attend in a sane, simple, and easy to use format. If you are
       an attendee of one of these conferences, I hope this site helps you as
-      well (and feel free to say hi if you see me walking around). If it does, I
-      love coffee ... and you can buy me one below :)
-    </Typography>
-    <Typography variant="body1" gutterBottom>
-      If you are developer and you would like to contribute a feature or add a
-      conference that I don&apos;t attend - I accept pull requests, so please
-      visit the{' '}
-      <a href="https://github.com/jgretz/conference-schedule">github repo</a>.
+      well (and feel free to say hi if you see me walking around). If it does,
+      and you are feeling generous, I love coffee ... and you can buy me one
+      below :)
     </Typography>
     <div className={classes.coffee}>
       <a
@@ -95,6 +116,20 @@ const About = ({classes}) => (
         <span className={classes.bmcText}>Buy me a coffee</span>
       </a>
     </div>
+
+    <Divider className={classes.divider} />
+
+    <Typography variant="body1" gutterBottom className={classes.text}>
+      If you&apos;re a developer and you would like to contribute a feature or
+      add a conference that I haven&apos;t - I gladly accept pull requests (
+      <Link
+        href="https://github.com/jgretz/conference-schedule"
+        target="_blank"
+      >
+        github repo
+      </Link>
+      ).
+    </Typography>
   </div>
 );
 
