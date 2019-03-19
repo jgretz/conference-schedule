bef2c882ff178dce512f715c9bddaca95f1521f7
Josh Gretz <jgretz@truefit.io>
2019-03-19 09:19:56 -0400

    fix selector for sessions, update it to the latest structure of favorites

--- site/app/features/schedule/selectors/sessionsForSelectedDaySelector.js
+++ site/app/features/schedule/selectors/sessionsForSelectedDaySelector.js
@@ -4,20 +4,22 @@ import {createSelector} from 'reselect';
 import selectedDaySelector from './selectedDaySelector';
 import sessionsSelector from './sessionsSelector';
 import favoritesSelector from './favoritesSelector';
+import selectedConferenceSelector from './selectedConferenceSelector';
 import {favoritesFilterSelector} from '../../shared/selectors';
 
 export default createSelector(
+  selectedConferenceSelector,
   selectedDaySelector,
   sessionsSelector,
   favoritesSelector,
   favoritesFilterSelector,
-  (day, allSessions, favorites, favoritesFilter) => {
+  (conference, day, allSessions, favorites, favoritesFilter) => {
     const sessions = (allSessions || []).filter(s => {
       if (!moment(s.startTime).isSame(day, 'day')) {
         return false;
       }
 
-      return !favoritesFilter || favorites.includes(s.id);
+      return !favoritesFilter || favorites[conference.title].includes(s.id);
     });
 
     return _.sortBy(sessions, s => moment(s.startTime));
