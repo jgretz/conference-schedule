abec3d9c08cf15b62533aa3a2d82d982982b2660
Josh Gretz <jgretz@truefit.io>
2019-07-26 15:48:36 -0400

    hotfix for private js

--- api/src/constants/index.js
+++ api/src/constants/index.js
@@ -1,9 +1,12 @@
-const PRIVATE = require('./private.js');
-const PRODUCTION = 'PRODUCTION';
+// const PRODUCTION = 'PRODUCTION';
 
 export default {
-  bitlyToken:
-    process.env.NODE_ENV === PRODUCTION
-      ? process.env.bitlyToken
-      : PRIVATE.default.bitlyToken,
+  bitlyToken: process.env.bitlyToken,
 };
+
+// export default {
+//   :
+//     process.env.NODE_ENV === PRODUCTION
+//       ?
+//       : PRIVATE.default.bitlyToken,
+// };
